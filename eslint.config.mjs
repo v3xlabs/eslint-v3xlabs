43edb89088c1a1fe967702c6d8624189857f024a
Luc <luc@lucemans.nl>
2026-02-13 20:53:22 +0100

    Lint lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,7 @@
-import eslintJs from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
-import pluginImport from 'eslint-plugin-import';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
-import eslintPluginSonarjs from 'eslint-plugin-sonarjs';
-import eslintPluginUnicorn from 'eslint-plugin-unicorn';
-import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
-import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import v3xlint from './lib/index.js';
 
-export default [
+export default defineConfig([
     {
         ignores: [
             '**/node_modules/**',
@@ -19,98 +12,5 @@ export default [
             'eslint.project2.config.mjs',
         ],
     },
-    eslintJs.configs.recommended,
-    ...tseslint.configs.recommended,
-    eslintPluginPrettierRecommended,
-    {
-        files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
-        plugins: {
-            import: pluginImport,
-            'simple-import-sort': eslintPluginSimpleImportSort,
-            'unused-imports': eslintPluginUnusedImports,
-            sonarjs: eslintPluginSonarjs,
-            unicorn: eslintPluginUnicorn,
-            '@stylistic': stylistic,
-        },
-        languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-        },
-        rules: {
-            'no-restricted-syntax': [
-                'error',
-                {
-                    selector: "Identifier[name='id']",
-                    message:
-                        'Use a descriptive identifier like user_id, blogpost_id, session_id, etc.',
-                },
-            ],
-            'import/no-default-export': 'error',
-            'import/no-duplicates': 'error',
-            'simple-import-sort/imports': 'error',
-            'simple-import-sort/exports': 'error',
-            'unused-imports/no-unused-imports': 'error',
-            'unused-imports/no-unused-vars': [
-                'warn',
-                {
-                    vars: 'all',
-                    varsIgnorePattern: '^_',
-                    args: 'after-used',
-                    argsIgnorePattern: '^_',
-                },
-            ],
-            quotes: ['error', 'single'],
-            semi: ['error', 'always'],
-            'no-var': 'error',
-            'prefer-const': 'error',
-            'linebreak-style': ['error', 'unix'],
-            'object-curly-spacing': ['error', 'always'],
-            'no-multiple-empty-lines': ['warn', { max: 2 }],
-            'max-lines': ['error', 500],
-            'no-control-regex': 'off',
-            '@typescript-eslint/no-empty-interface': 'off',
-            '@typescript-eslint/no-explicit-any': 'warn',
-            '@typescript-eslint/ban-ts-comment': 'off',
-            '@typescript-eslint/no-non-null-assertion': 'off',
-            '@typescript-eslint/no-namespace': 'off',
-            '@typescript-eslint/no-unused-vars': 'off',
-            'sonarjs/cognitive-complexity': ['error', 15],
-            'sonarjs/no-duplicate-string': ['error', { threshold: 3 }],
-            'sonarjs/no-identical-functions': 'error',
-            'unicorn/no-array-for-each': 'off',
-            'unicorn/no-array-reduce': 'off',
-            'unicorn/prefer-at': 'off',
-            'unicorn/prefer-module': 'off',
-            'unicorn/prefer-node-protocol': 'error',
-            'unicorn/no-array-callback-reference': 'off',
-            'unicorn/prefer-spread': 'off',
-            'unicorn/prevent-abbreviations': 'off',
-            'unicorn/expiring-todo-comments': 'off',
-            'unicorn/filename-case': 'off',
-            '@stylistic/padding-line-between-statements': [
-                'error',
-                {
-                    blankLine: 'always',
-                    prev: '*',
-                    next: ['return', 'if', 'switch', 'try', 'for'],
-                },
-                {
-                    blankLine: 'always',
-                    prev: ['if', 'switch', 'try', 'const', 'let'],
-                    next: '*',
-                },
-                {
-                    blankLine: 'any',
-                    prev: ['const', 'let'],
-                    next: ['const', 'let'],
-                },
-            ],
-        },
-    },
-    {
-        files: ['src/index.ts'],
-        rules: {
-            'import/no-default-export': 'off',
-        },
-    },
-];
+    ...v3xlint.configs['recommended'],
+]);
--- src/index.ts
+++ src/index.ts
@@ -1,193 +1,203 @@
-import eslint from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
-import type { ESLint, Linter } from 'eslint';
-import pluginImport from 'eslint-plugin-import';
-import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
-import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
-import eslintPluginTailwindcss from 'eslint-plugin-tailwindcss';
-import eslintPluginUnicorn from 'eslint-plugin-unicorn';
-import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
+import type { ESLint, Linter } from "eslint";
+import pluginImport from "eslint-plugin-import";
+import eslintPluginReactHooks from "eslint-plugin-react-hooks";
+import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
+import eslintPluginTailwindcss from "eslint-plugin-tailwindcss";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import eslintPluginUnusedImports from "eslint-plugin-unused-imports";
+import tseslint from "typescript-eslint";
 
 const sortVitest: Linter.Config = {
-    files: [
-        '**/vitest.config.{js,ts,mts}',
-        '**/wxt.config.{js,ts,mts}',
-        '**/vite.config.{js,ts,mts}',
-    ],
-    rules: {
-        'import/no-default-export': 'off',
-    },
+  files: [
+    "**/vitest.config.{js,ts,mts}",
+    "**/wxt.config.{js,ts,mts}",
+    "**/vite.config.{js,ts,mts}",
+  ],
+  rules: {
+    "import/no-default-export": "off",
+  },
 };
 
 const tsImportSort: Linter.Config = {
-    ignores: ['**/dist/**', '**/out/**', '**/node_modules/**'],
-    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx,mts}'],
-    plugins: {
-        import: pluginImport,
-        'simple-import-sort': eslintPluginSimpleImportSort,
-        'unused-imports': eslintPluginUnusedImports,
-    },
-    languageOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    rules: {
-        'import/no-default-export': 'error',
-        'import/no-duplicates': 'error',
-        'simple-import-sort/imports': 'error',
-        'simple-import-sort/exports': 'error',
-        'unused-imports/no-unused-imports': 'error',
-        'unused-imports/no-unused-vars': [
-            'warn',
-            {
-                vars: 'all',
-                varsIgnorePattern: '^_',
-                args: 'after-used',
-                argsIgnorePattern: '^_',
-            },
-        ],
-    },
+  ignores: ["**/dist/**", "**/out/**", "**/node_modules/**"],
+  files: ["**/*.{js,mjs,cjs,jsx,ts,tsx,mts}"],
+  plugins: {
+    "import": pluginImport,
+    "simple-import-sort": eslintPluginSimpleImportSort,
+    "unused-imports": eslintPluginUnusedImports,
+  },
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "import/no-default-export": "error",
+    "import/no-duplicates": "error",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
 };
 
-const tsStylistic: Linter.Config = stylistic.configs['recommended-flat'];
+const tsStylistic: Linter.Config = stylistic.configs["recommended-flat"];
 
 const tsJsEs: Linter.Config[] = [
-    {
-        rules: eslint.configs.recommended.rules,
-    },
-    ...tseslint.configs.recommended,
+  {
+    rules: eslint.configs.recommended.rules,
+  },
+  ...tseslint.configs.recommended,
 ];
 
 const tsOther: Linter.Config = {
-    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx,mts}'],
-    plugins: {
-        unicorn: eslintPluginUnicorn,
-    },
-    languageOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    rules: {
-        ...eslintPluginUnicorn.configs.recommended.rules,
-        'no-restricted-syntax': [
-            'error',
-            {
-                selector: 'Identifier[name="id"]',
-                message:
-                    'Use a descriptive identifier like user_id, blogpost_id, session_id, etc.',
-            },
-        ],
-        'simple-import-sort/imports': 'error',
-        'simple-import-sort/exports': 'error',
-        'unused-imports/no-unused-imports': 'error',
-        'unused-imports/no-unused-vars': [
-            'warn',
-            {
-                vars: 'all',
-                varsIgnorePattern: '^_',
-                args: 'after-used',
-                argsIgnorePattern: '^_',
-            },
-        ],
-        quotes: ['error', 'double'],
-        semi: ['error', 'always'],
-        'no-var': 'error',
-        'prefer-const': 'error',
-        'func-style': ['error', 'expression'],
-        'prefer-arrow-callback': ['error'],
-        'arrow-body-style': ['error', 'as-needed'],
-        'linebreak-style': ['error', 'unix'],
-        'object-curly-spacing': ['error', 'always'],
-        'no-multiple-empty-lines': ['warn', { max: 2 }],
-        'max-lines': ['error', 500],
-        'no-control-regex': 'off',
-        'no-nested-ternary': 'off',
-        'no-unneeded-ternary': 'warn',
-        'unicorn/no-array-for-each': 'off',
-        'unicorn/no-array-reduce': 'off',
-        'unicorn/prefer-at': 'off',
-        'unicorn/prefer-module': 'off',
-        'unicorn/prefer-node-protocol': 'error',
-        'unicorn/no-array-callback-reference': 'off',
-        'unicorn/prefer-spread': 'off',
-        'unicorn/prevent-abbreviations': 'off',
-        'unicorn/expiring-todo-comments': 'off',
-        'unicorn/filename-case': 'off',
-        '@stylistic/quotes': ['error', 'double'],
-        '@stylistic/semi': ['error', 'always'],
-        '@stylistic/no-confusing-arrow': 'error',
-        '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
-        '@stylistic/newline-per-chained-call': 'error',
-        '@stylistic/padding-line-between-statements': [
-            'error',
-            {
-                blankLine: 'always',
-                prev: '*',
-                next: ['return', 'if', 'switch', 'try', 'for'],
-            },
-            {
-                blankLine: 'always',
-                prev: ['if', 'switch', 'try', 'const', 'let'],
-                next: '*',
-            },
-            {
-                blankLine: 'any',
-                prev: ['const', 'let'],
-                next: ['const', 'let'],
-            },
-        ],
-        '@stylistic/member-delimiter-style': [
-            'error',
-            {
-                multiline: {
-                    delimiter: 'semi',
-                    requireLast: true,
-                },
-                singleline: {
-                    delimiter: 'semi',
-                    requireLast: true,
-                },
-            },
-        ],
-    },
+  files: ["**/*.{js,mjs,cjs,jsx,ts,tsx,mts}"],
+  plugins: {
+    unicorn: eslintPluginUnicorn,
+  },
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    ...eslintPluginUnicorn.configs.recommended.rules,
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: "Identifier[name=\"id\"]",
+        message:
+                    "Use a descriptive identifier like user_id, blogpost_id, session_id, etc.",
+      },
+    ],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"],
+    "no-var": "error",
+    "prefer-const": "error",
+    "func-style": ["error", "expression"],
+    "prefer-arrow-callback": ["error"],
+    "arrow-body-style": ["error", "as-needed"],
+    "linebreak-style": ["error", "unix"],
+    "object-curly-spacing": ["error", "always"],
+    "no-multiple-empty-lines": ["warn", { max: 2 }],
+    "max-lines": ["error", 500],
+    "no-control-regex": "off",
+    "no-nested-ternary": "off",
+    "no-unneeded-ternary": "warn",
+    "unicorn/no-array-for-each": "off",
+    "unicorn/no-array-reduce": "off",
+    "unicorn/prefer-at": "off",
+    "unicorn/prefer-module": "off",
+    "unicorn/prefer-node-protocol": "error",
+    "unicorn/no-array-callback-reference": "off",
+    "unicorn/prefer-spread": "off",
+    "unicorn/prevent-abbreviations": "off",
+    "unicorn/expiring-todo-comments": "off",
+    "unicorn/filename-case": "off",
+    "@stylistic/quotes": ["error", "double"],
+    "@stylistic/semi": ["error", "always"],
+    "@stylistic/no-confusing-arrow": "error",
+    "@stylistic/no-multiple-empty-lines": ["error", { max: 1 }],
+    "@stylistic/newline-per-chained-call": "error",
+    "@stylistic/padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "*",
+        next: ["return", "if", "switch", "try", "for"],
+      },
+      {
+        blankLine: "always",
+        prev: ["if", "switch", "try", "const", "let"],
+        next: "*",
+      },
+      {
+        blankLine: "any",
+        prev: ["const", "let"],
+        next: ["const", "let"],
+      },
+    ],
+    "@stylistic/member-delimiter-style": [
+      "error",
+      {
+        multiline: {
+          delimiter: "semi",
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: "semi",
+          requireLast: true,
+        },
+      },
+    ],
+  },
 };
 
 const tsReact: Linter.Config = {
-    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx,mts}'],
-    languageOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: {
-        'react-hooks': eslintPluginReactHooks as ESLint.Plugin,
-        tailwindcss: eslintPluginTailwindcss as ESLint.Plugin,
-    },
-    rules: {
-        ...eslintPluginReactHooks.configs['recommended-latest'].rules,
-        ...eslintPluginTailwindcss.configs['flat/recommended'].rules,
-    },
+  files: ["**/*.{js,mjs,cjs,jsx,ts,tsx,mts}"],
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: {
+    "react-hooks": eslintPluginReactHooks as ESLint.Plugin,
+  },
+  rules: {
+    ...eslintPluginReactHooks.configs["recommended-latest"].rules,
+  },
+};
+
+const tsTailwindcss: Linter.Config = {
+  files: ["**/*.{js,mjs,cjs,jsx,ts,tsx,mts}"],
+  plugins: {
+    tailwindcss: eslintPluginTailwindcss as ESLint.Plugin,
+  },
+  rules: {
+    ...eslintPluginTailwindcss.configs["flat/recommended"].rules,
+  },
 };
 
 const plugin: ESLint.Plugin & {
-    configs: {
-        sort: Linter.Config[];
-        recommended: Linter.Config[];
-        react: Linter.Config[];
-    };
+  configs: {
+    sort: Linter.Config[];
+    recommended: Linter.Config[];
+    react: Linter.Config[];
+    tailwindcss: Linter.Config[];
+  };
 } = {
-    rules: {},
-    configs: {
-        sort: [sortVitest, tsImportSort],
-        recommended: [
-            ...tsJsEs,
-            tsStylistic,
-            tsImportSort,
-            sortVitest,
-            tsOther,
-        ],
-        react: [tsReact],
-    },
+  rules: {},
+  configs: {
+    sort: [sortVitest, tsImportSort],
+    recommended: [
+      ...tsJsEs,
+      tsStylistic,
+      tsImportSort,
+      sortVitest,
+      tsOther,
+    ],
+    react: [tsReact],
+    tailwindcss: [tsTailwindcss],
+  },
 };
 
 export default plugin;
